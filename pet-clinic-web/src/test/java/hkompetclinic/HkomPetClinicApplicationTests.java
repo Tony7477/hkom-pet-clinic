f42db8f70be36b495526258f2c71060ce87f9097
hariharan <hariharan7477@gmail.com>
2022-11-14 22:49:01 +0530

    sort error plus edit the pom in pet-clinic-data.

--- pet-clinic-web/src/test/java/hkompetclinic/HkomPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/hkompetclinic/HkomPetClinicApplicationTests.java
@@ -1,9 +1,10 @@
 package hkompetclinic;
 
+import hkom.springframework.hkompetclinic.HkomPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HkomPetClinicApplication.class)
 class HkomPetClinicApplicationTests {
 
 	@Test
