bda434e89889d12431719d232bed93bdf08e64a5
hariharan <hariharan7477@gmail.com>
2022-11-14 14:13:01 +0530

    Adding POJO's.Closes #2.

---
+++ src/main/java/hkom/springframework/hkompetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package hkom.springframework.hkompetclinic.model;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
---
+++ src/main/java/hkom/springframework/hkompetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package hkom.springframework.hkompetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
